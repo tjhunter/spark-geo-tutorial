36503d6188bdaa54bdd5248ca314676af213438f
Timothy Hunter <tjhunter@eecs.berkeley.edu>
2012-07-13 09:39:07 -0700

    working with access keys, a bug somewhere (in spark?)

--- deploy-jars.sh
+++ deploy-jars.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Small script to deploy jar files of a project in a mesos environment
-all_jars=`./sbt/sbt get-jars`
+all_jars=`./sbt/sbt get-jars | grep .jar`
+split_jars=${all_jars//":"/" "}
